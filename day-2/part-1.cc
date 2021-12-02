101fc9c351972926eac714d6521aa2fc2b35aa0c
Sushain Cherivirala <sushain97@gmail.com>
2021-12-01 21:23:17 -0800

    Inital commit

---
+++ day-2/part-1.cc
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  std::ifstream input_file(argv[1]);
+
+  std::string direction; 
+  int units;
+
+  int horiz = 0, depth = 0;
+  
+  while (input_file >> direction >> units) {
+    if (direction == "forward") {
+      horiz += units;
+    } else if (direction == "down") {
+      depth += units;
+    } else if (direction == "up") {
+      depth -= units;
+    }
+  }
+
+  std::cout << horiz * depth << "\n";
+}
---
+++ day-2/part-2.cc
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  std::ifstream input_file(argv[1]);
+
+  std::string direction; 
+  int units;
+
+  int horiz = 0, depth = 0, aim = 0;
+  
+  while (input_file >> direction >> units) {
+    if (direction == "forward") {
+      horiz += units;
+      depth += aim * units;
+    } else if (direction == "down") {
+      aim += units;
+    } else if (direction == "up") {
+      aim -= units;
+    }
+  }
+
+  std::cout << horiz * depth << "\n";
+}
