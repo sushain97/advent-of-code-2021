5e34dc4a49de79a94b000d08d2520fabe0d6c96d
Sushain Cherivirala <sushain@skc.name>
2021-12-03 14:56:20 -0800

    Rewrite day-2 in shell

---
+++ day-2/part-1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+
+horiz=depth=0
+
+while read -r line; do    
+  IFS=' '; set -- $line; unset IFS
+  case $1 in
+    'forward') horiz=$((horiz + $2)) ;;
+    'down') depth=$((depth + $2)) ;;
+    'up') depth=$((depth - $2)) ;;
+  esac
+done < "$1"
+
+echo "$((horiz * depth))"
