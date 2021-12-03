167a2921ea787da4976e5d0c68f89ca592331be9
Sushain Cherivirala <sushain97@gmail.com>
2021-12-02 23:56:39 -0800

    fmt

--- day-2/part-1.cc
+++ day-2/part-1.cc
@@ -1,14 +1,14 @@
 #include <fstream>
 #include <iostream>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   std::ifstream input_file(argv[1]);
 
-  std::string direction; 
+  std::string direction;
   int units;
 
   int horiz = 0, depth = 0;
-  
+
   while (input_file >> direction >> units) {
     if (direction == "forward") {
       horiz += units;
