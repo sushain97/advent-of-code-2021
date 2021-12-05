fca75b5e887a3dc511e23d6d088235ddcb3df5d9
Sushain Cherivirala <sushain@skc.name>
2021-12-04 21:52:47 -0800

    Rm unused

--- day-4/Part1.java
+++ day-4/Part1.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
--- day-4/Part2.java
+++ day-4/Part2.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
