eaaf3ba2bede0d7edcca219cfec2f0c050c316a0
Sushain Cherivirala <sushain@skc.name>
2021-12-09 00:46:39 -0800

    Slightly neater Python

--- day-9/part-1.py
+++ day-9/part-1.py
@@ -1,21 +1,22 @@
 import sys
 
 if __name__ == "__main__":
-    lines = [s.strip() for s in open(sys.argv[1]).readlines()]
+    with open(sys.argv[1]) as f:
+        lines = f.read().splitlines()
+        grid = [
+            [9] * (len(lines[0]) + 2),
+            *[[9, *map(int, l), 9] for l in lines],
+            [9] * (len(lines[0]) + 2),
+        ]
 
-    grid = (
-        [[9] * (len(lines[0]) + 2)]
-        + [[9] + list(map(int, l)) + [9] for l in lines]
-        + [[9] * (len(lines[0]) + 2)]
-    )
-
-    print(
-        sum(
-            grid[x][y]
-            for x in range(1, len(grid) - 1)
-            for y in range(1, len(lines[0]) + 1)
-            if all(
-                grid[x][y] < grid[x + dx][y + dy] for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        print(
+            sum(
+                grid[x][y]
+                for x in range(1, len(grid) - 1)
+                for y in range(1, len(lines[0]) + 1)
+                if all(
+                    grid[x][y] < grid[x + dx][y + dy]
+                    for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]
+                )
             )
         )
-    )
--- day-9/part-2.py
+++ day-9/part-2.py
@@ -2,34 +2,36 @@ import collections
 import sys
 
 if __name__ == "__main__":
-    lines = [s.strip() for s in open(sys.argv[1]).readlines()]
+    with open(sys.argv[1]) as f:
+        lines = f.read().splitlines()
+        grid = [
+            [9] * (len(lines[0]) + 2),
+            *[[9, *map(int, l), 9] for l in lines],
+            [9] * (len(lines[0]) + 2),
+        ]
 
-    grid = (
-        [[9] * (len(lines[0]) + 2)]
-        + [[9] + list(map(int, l)) + [9] for l in lines]
-        + [[9] * (len(lines[0]) + 2)]
-    )
+        low_points = {
+            (x, y)
+            for x in range(1, len(grid) - 1)
+            for y in range(1, len(lines[0]) + 1)
+            if all(
+                grid[x][y] < grid[x + dx][y + dy] for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]
+            )
+        }
 
-    low_points = {
-        (x, y)
-        for x in range(1, len(grid) - 1)
-        for y in range(1, len(lines[0]) + 1)
-        if all(grid[x][y] < grid[x + dx][y + dy] for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)])
-    }
+        basins = collections.defaultdict(int)
+        for x in range(1, len(grid) - 1):
+            for y in range(1, len(lines[0]) + 1):
+                if grid[x][y] != 9:
+                    i, j = x, y
 
-    basins = collections.defaultdict(int)
-    for x in range(1, len(grid) - 1):
-        for y in range(1, len(lines[0]) + 1):
-            if grid[x][y] != 9:
-                i, j = x, y
+                    while (i, j) not in low_points:
+                        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                            if grid[i + dx][j + dy] < grid[i][j]:
+                                i, j = i + dx, j + dy
+                                break
 
-                while (i, j) not in low_points:
-                    for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                        if grid[i + dx][j + dy] < grid[i][j]:
-                            i, j = i + dx, j + dy
-                            break
+                    basins[(i, j)] += 1
 
-                basins[(i, j)] += 1
-
-    a, b, c, *rest = sorted(basins.values(), reverse=True)
-    print(a * b * c)
+        a, b, c, *rest = sorted(basins.values(), reverse=True)
+        print(a * b * c)
