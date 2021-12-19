b4c79b87a5d69dade944a732b8d86d8e8c3c6638
Sushain Cherivirala <sushain@skc.name>
2021-12-19 12:34:54 -0800

    Slightly faster Python

--- day-19/part-2.py
+++ day-19/part-2.py
@@ -15,40 +15,40 @@ ROTATIONS = list(
 
 @dataclasses.dataclass
 class Scanner:
-    beacons: set[tuple]
+    beacons: list[tuple]
     position: tuple = None
 
 
 def test_scanner(unknown_scanner, known_scanner):
     for (orientation, permutation) in ROTATIONS:
-        counts = collections.Counter()
+        counts = collections.defaultdict(int)
 
         for unknown_beacon in unknown_scanner.beacons:
             for known_beacon in known_scanner.beacons:
-                position = tuple(
-                    unknown_beacon[coordinate]
-                    + orientation[coordinate] * known_beacon[permutation[coordinate]]
-                    for coordinate in range(3)
+                position = (
+                    unknown_beacon[0] + orientation[0] * known_beacon[permutation[0]],
+                    unknown_beacon[1] + orientation[1] * known_beacon[permutation[1]],
+                    unknown_beacon[2] + orientation[2] * known_beacon[permutation[2]],
                 )
                 counts[position] += 1
 
                 if counts[position] >= 12:
                     known_scanner.position = position
-                    known_scanner.beacons = {
+                    known_scanner.beacons = [
                         tuple(
                             known_scanner.position[coordinate]
                             - orientation[coordinate] * beacon[permutation[coordinate]]
                             for coordinate in range(3)
                         )
                         for beacon in known_scanner.beacons
-                    }
+                    ]
                     return True
 
 
 if __name__ == "__main__":
     scanners = [
         Scanner(
-            {tuple(map(int, line.strip().split(","))) for line in scanner_lines.splitlines()[1:]}
+            [tuple(map(int, line.strip().split(","))) for line in scanner_lines.splitlines()[1:]]
         )
         for scanner_lines in open(sys.argv[1]).read().split("\n\n")
     ]
